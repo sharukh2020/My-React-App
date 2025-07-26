616a105d40cbb61bd70e2d5d6616b0209ece8caa
sharukh2020 <sharukhkhana@gmail.com>
2025-07-26 19:52:25 +0530

    new

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBVRO065yF4P3vgzP5YDohfvkQ2Q8E6jvo",
-  authDomain: "sign-up-and-sign-in-and-state.firebaseapp.com",
-  projectId: "sign-up-and-sign-in-and-state",
-  storageBucket: "sign-up-and-sign-in-and-state.firebasestorage.app",
-  messagingSenderId: "540834471983",
-  appId: "1:540834471983:web:f7294950ce090c612e66da"
+  apiKey: "AIzaSyAR7A3nTRKr-1U90bc2AO3IfybEX02Qrys",
+  authDomain: "appcheck-test-f572e.firebaseapp.com",
+  projectId: "appcheck-test-f572e",
+  storageBucket: "appcheck-test-f572e.firebasestorage.app",
+  messagingSenderId: "941402830108",
+  appId: "1:941402830108:web:a3193d31714d00e65736d9"
 };
 
 // Initialize Firebase
